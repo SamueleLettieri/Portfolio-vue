da0bfb74bf6b9db629b29db39b8f4d83d959c1d9
SamueleLettieri <samuele2073@gmail.com>
2023-06-07 17:35:10 +0200

    responsive

--- src/components/Main/AboutMeMain.vue
+++ src/components/Main/AboutMeMain.vue
@@ -1,17 +1,17 @@
 <template>
   <section id="About-Me" class="container pt-5 ms_margin">
     <div>
-        <div class="row mt-5">
+        <div class="row">
         <div class="col-lg-6 col-12">
             <h2 class="d-inline-block px-3 rounded-2">Chi Sono</h2>
-            <div class="ps-5">
+            <div class="ps-3">
                 <p>Siccome vivevo in una località turistica, sono entrato nel mondo del lavoro durante il periodo delle superiori, cominciando da una piccola pasticceria e cambiando di stagione in stagione un locale, non era una vera e propria aspirazione.</p>
                 <p>Al mio quinto anno di alberghiero la mia scuola mise in palio una borsa di studio per i migliori 4 studenti dell'istituto, e decisi di sfruttare questa opportunità per realizzare la mia grande passione di lavorare nell'informatica, mi impegnai tanto per vincere la borsa di studio e alla fine ci riuscì.</p>
                 <p>Sono sempre stato interessato alla tecnologia e all'informatica.</p>
                 <p>Decisi di iscrivermi all'università Federico II di Napoli al corso di ingegneria informatica, e consolidare le basi pratiche frequentando un corso intensivo per diventare Full Stack Web Developer!</p>
             </div>
         </div>
-        <div class="col-lg-6 text-center col-12"><img src="../../assets/samImg.jpg" alt=""></div>
+        <div class="col-lg-6 text-center col-12 "><img src="../../assets/samImg.jpg" alt=""></div>
         </div>
     </div>    
   </section>
--- src/components/Main/WorksMain.vue
+++ src/components/Main/WorksMain.vue
@@ -4,7 +4,7 @@
         <div class="row justify-content-between ms_center">
             <div v-for="(card, index) in cards" :key="index" class="col-7 col-sm-5 col-lg-3 ms_p-0 mb-5 mx-3">
                 <div @click="nextIndex(index), trueFalse()"  class="card w-100" style="cursor: pointer;">
-                    <img class="ms_img card-img-top" :src="card.img" alt="Card image cap">
+                    <img class="card-img-top" :src="card.img" alt="Card image cap">
                     <div class="card-body">
                         <h3 class="card-text">{{ card.title }}</h3>
                     </div>
@@ -12,25 +12,27 @@
             </div>
         </div>    
         <div class="ms_f" v-if="isTrue">
-            <div class="text-end p-5">
-                <span class="d-inline-block mt-2">
-                    <i class="p-2 ms-4 rounded-5 fa-solid fa-x fs-3 ms_x ms_xe" @click="xWork()"></i>
+            <div class="text-end p-1">
+                <span class="d-inline-block">
+                    <i class="p-2 ms-4 rounded-5 fa-solid fa-x fs-3 mx-3 ms_x ms_xe ms_icon" :class="cards[activeIndex].class" @click="xWork()"></i>
                 </span>
             </div>   
-            <div>
-                <div class="ms_img d-inline-block"><img class="rounded-5" :src="cards[activeIndex].img" alt=""></div> 
-                <div class="ms_text d-inline-block rounded-3 p-4">
-                    <h3 class="mb-3">{{ cards[activeIndex].title }}</h3>
-                    <p class="fs-5 mb-3">{{ cards[activeIndex].description }}</p>
-                    <div class="fs-5 mb-3">{{ cards[activeIndex].tecnologie }}</div>
+            <div class="row justify-content-center">
+                <div class="col-lg-7 col-md-8  col-10"><img class="rounded-5" :src="cards[activeIndex].img" alt=""></div> 
+                <div class="col-lg-4 col-md-4  col-10 pt-3 position-relative">
+                    <div class="ms_text  rounded-3 p-4" :class="cards[activeIndex].class">
+                        <h3 class="mb-3">{{ cards[activeIndex].title }}</h3>
+                        <p class="fs-5 mb-3">{{ cards[activeIndex].description }}</p>
+                        <div class="fs-5 mb-3">{{ cards[activeIndex].tecnologie }}</div>
+                    </div>
                 </div>
             </div> 
-            <div class="">
+            <div class="position-fixed ms_bottom mb-2">
                 <span class="d-inline-block col-5">
-                    <i class="p-2 ms-4 rounded-5 fa-solid fa-arrow-left fs-3 ms_arrow ms_x" @click="beforeWork()"></i>
+                    <i class="p-2 ms-4 rounded-5 fa-solid fa-arrow-left fs-3 ms_icon ms_x" :class="cards[activeIndex].class" @click="beforeWork()"></i>
                 </span>
                 <span class="d-inline-block ms_w text-end">
-                    <i class="fa-solid fa-arrow-right fs-3 ms_arrow p-2  rounded-5 ms_x" @click="nextWork()"></i>
+                    <i class="fa-solid fa-arrow-right fs-3 p-2 rounded-5 ms_icon ms_x" :class="cards[activeIndex].class" @click="nextWork()"></i>
                 </span>
             </div>   
         </div>
@@ -52,24 +54,28 @@ export default {
                     title: 'Comics',
                     description: 'Una pagina che riproduce il sito web originale dei fumetti DC creato utilizzando VueJs. Ogni sezione è composta da diversi componenti e popolata da array di oggetti',
                     tecnologie: 'Tecnologie: Html, SCSS, Bootstrap, JavaScript, Vue.',
+                    class: 'ms_blue',
                 },
                 {
                     img: require('@/assets/discord-img.png'),
                     title: 'Discord',
                     description: 'Una pagina Web di base che riproduce la Home page di Discord utilizando le prime prorpieta di css. Riproducendo prima uno scheletro della pagina e in seguito aggiunti i contenuti',
                     tecnologie: 'Tecnologie: Html, CSS.',
+                    class: 'ms_blue',
                 },
                 {
                     img: require('@/assets/Immagine 2022-10-06 182244.png'),
                     title: 'Dropbox',
                     description: 'Un altro progetto front-end realizzato con Html e CSS. Dropbox il servizio più diffuso di file hosting.',
                     tecnologie: 'Tecnologie: Html, CSS.',
+                    class: 'ms_light_grey',
                 },
                 {
                     img: require('@/assets/deliveboo.png'),
                     title: 'Deliveboo',
                     description: 'Progetto sviluppato in team, una pagina web che prende spunto da deliveroo. Progetto curato sia nella parte back-end che front-end permettendo al ristoratore di gestire piatti e ordini, e al cliente di effetuare ordini e ricerche incrociate.',
                     tecnologie: 'Tecnologie: MySql, PHP, Laravel, Htm, CSS, JavaScript, Vue',
+                    class: 'ms_red',
                 },/* 
                 {
                     img: require('@/assets/sony-img.png'),
@@ -150,37 +156,51 @@ export default {
     z-index: 2;
     top: 0;
     left: 0;
+    overflow: hidden;
 
     img{
         width: 100%;
         
     }
 
-    .ms_img{
-        width: 60%;
-        position: relative;
-        top: 0px;
-        left: 100px;
-    }
 
     .ms_text{
-        background-color: lightgray;
-        position: relative;
-        top: 400px;
-        right: 50px;
-        width: 30%;
+        color: white;
     }
 
     .ms_w{
         width: 55%;
     }
 
-    .ms_arrow:hover{
-        border: 2px solid black;
+    .ms_blue{
+        border: 2px solid #006EFF;
+        background-color: #006EFF;
+        color: white;
+    }
+
+    .ms_light_grey{
+        border: 2px solid lightgray;
+        background-color: lightgrey;
+        color: black;
+    }
+
+    .ms_red{
+        border: 2px solid red;
+        background-color: red;
+        color: white;
+    }
+
+    .ms_icon:hover{
+        border: 2px solid white;
     }
 
     .ms_x{
         cursor: pointer;
+    }
+
+    .ms_bottom{
+        bottom: 0;
+        width: 100%;
     }
 }
 
@@ -190,4 +210,24 @@ export default {
         justify-content: center !important;
     }
 }
+
+
+@media screen and (min-width: 768px) {
+
+    .ms_text{
+        position: absolute;
+        top: 150px;
+        left: -50px;
+    }
+
+}
+
+@media screen and (min-width: 1200px) {
+
+    .ms_text{
+        position: absolute;
+        top: 200px;
+        left: -50px;
+    }
+} 
 </style>
